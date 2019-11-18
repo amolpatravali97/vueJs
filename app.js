68cde1e9edc23da0157cf56de5a8eb2c263e63da
Xxamol108xX <amol.patravali97@gmail.com>
2019-11-18 08:14:43 +0530

    Onclick event

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+const colors = [
+  'red',
+  'blue',
+  'green',
+  'yellow',
+  'purple',
+  'black',
+  'brown',
+  'violet',
+  'orange',
+  'pink'
+];
+
 const app = new Vue({
   el: '#app',
   data: {
     message: 'My first Vue app!',
-    styleInsert: 'color: blue; font-weight: bold'
+    styleInsert: 'color: blue'
+  },
+  methods: {
+    changeColor() {
+      this.styleInsert = `color: ${colors[Math.round(Math.random() * 10)]}`
+    },
   },
 });
+
