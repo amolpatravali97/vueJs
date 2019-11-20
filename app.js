6f1e27f73551458a27ac423980fd206be563bb79
Xxamol108xX <amol.patravali97@gmail.com>
2019-11-21 01:09:15 +0530

    Looping through

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
-const colors = [
-  'red',
-  'blue',
-  'green',
-  'yellow',
-  'purple',
-  'black',
-  'brown',
-  'violet',
-  'orange',
-  'pink'
-];
-
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'My first Vue app!',
-    styleInsert: 'color: blue'
-  },
-  methods: {
-    changeColor() {
-      this.styleInsert = `color: ${colors[Math.round(Math.random() * 10)]}`
-    },
+    rainbowColors: [
+      { name: 'Violet' },
+      { name: 'Indigo' },
+      { name: 'Blue' },
+      { name: 'Green' },
+      { name: 'Yellow' },
+      { name: 'Orange' },
+      { name: 'Red' },
+    ],
   },
 });
 
